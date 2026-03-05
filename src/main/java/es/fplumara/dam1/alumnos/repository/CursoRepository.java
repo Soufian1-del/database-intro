174fa7571412d6987811d3d6b354e377bffcfcba
sourrxz <sourrxz@gmail.com>
2026-03-05 10:14:49 +0100

    hecho el ejercicio de practica de postgress

---
+++ src/main/java/es/fplumara/dam1/alumnos/repository/CursoRepository.java
@@ -0,0 +1,21 @@
+package es.fplumara.dam1.alumnos.repository;
+
+import es.fplumara.dam1.alumnos.model.Curso;
+
+import java.util.List;
+
+public interface CursoRepository {
+
+    void initSchema();
+
+
+    void crearCurso(int id, String nombre, Boolean activo);
+
+    void activar(int id);
+
+    void eliminarSiNombreContiene(String nombre);
+
+    List<Curso> listarPorEstado(boolean activo);
+
+    List<Curso> listarOrdenadoPorCampo();
+}
---
+++ src/main/java/es/fplumara/dam1/alumnos/service/CursoService.java
@@ -0,0 +1,17 @@
+package es.fplumara.dam1.alumnos.service;
+
+import es.fplumara.dam1.alumnos.model.Curso;
+
+import java.util.List;
+
+public interface CursoService {
+    void initSchema();
+
+    void activar(int id);
+
+    void eliminarSiNombreContiene(String nombre);
+
+    List<Curso> listarPorEstado(boolean activo);
+
+    List<Curso> listarOrdenadoPorCampo();
+}
